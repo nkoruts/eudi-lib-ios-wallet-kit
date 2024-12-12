e04089b2c50d7b71246acf2e8fb5d418b65df769
Nikita Koruts <nikitakoruts@gmail.com>
2024-12-12 13:32:46 +0200

    Add createSigner method

--- Sources/EudiWalletKit/Extensions/BindingKey+Ext.swift
+++ Sources/EudiWalletKit/Extensions/BindingKey+Ext.swift
@@ -10,10 +10,14 @@ import OpenID4VCI
 import JOSESwift
 
 public extension BindingKey {
-    public func getProof() throws -> String? {
+    func getProof() async throws -> String? {
         switch self {
         case .jwk(let algorithm, let jwk, let privateKey, _):
-            return try generateProof(algorithm: algorithm, jwk: jwk, privateKey: privateKey)
+            return try await generateProof(
+                algorithm: algorithm,
+                jwk: jwk,
+                privateKey: privateKey
+            )
         default:
             return nil
         }
@@ -23,11 +27,7 @@ public extension BindingKey {
         algorithm: JWSAlgorithm,
         jwk: JWK,
         privateKey: SigningKeyProxy
-    ) throws -> String? {
-        
-        guard case let .secKey(secKey) = privateKey else {
-            throw ValidationError.error(reason: "Wrong security key")
-        }
+    ) async throws -> String? {
         
         let header = try JWSHeader(parameters: [
             "typ": "openid4vci-proof+jwt",
@@ -42,10 +42,13 @@ public extension BindingKey {
         guard let signatureAlgorithm = SignatureAlgorithm(rawValue: algorithm.name) else {
             throw CredentialIssuanceError.cryptographicAlgorithmNotSupported
         }
-        guard let signer = Signer(signatureAlgorithm: signatureAlgorithm, key: secKey) else {
-            throw ValidationError.error(reason: "Unable to create JWS signer")
-        }
         
+        let signer = try await createSigner(
+            with: header,
+            and: payload,
+            for: privateKey,
+            and: signatureAlgorithm
+        )
         let jws = try JWS(
             header: header,
             payload: payload,
@@ -53,5 +56,56 @@ public extension BindingKey {
         )
         
         return jws.compactSerializedString
+    }
+    
+    private func createSigner(
+        with header: JWSHeader,
+        and payload: Payload,
+        for privateKey: SigningKeyProxy,
+        and signatureAlgorithm: SignatureAlgorithm
+    ) async throws -> Signer {
+        switch privateKey {
+        case .secKey(let secKey):
+            guard let secKeySigner = Signer(
+                signatureAlgorithm: signatureAlgorithm,
+                key: secKey)
+            else {
+                throw ValidationError.error(reason: "Failed creation of the secKey signer")
+            }
+             return secKeySigner
+        case .custom(let customAsyncSigner):
+            let signingInput: Data? = [
+                header as DataConvertible,
+                payload as DataConvertible
+            ]
+            .map { $0.data().base64URLEncodedString() }
+            .joined(separator: ".")
+            .data(using: .ascii)
+            
+            guard let signingInput = signingInput else {
+                throw ValidationError.error(reason: "Invalid signing input for signing data")
+            }
+            
+            let signature = try await customAsyncSigner.signAsync(signingInput)
+            let customSigner = CustomSigner(
+                signature: signature,
+                algorithm: signatureAlgorithm
+            )
+            return Signer(customSigner: customSigner)
+        }
+    }
+}
+
+class CustomSigner: JOSESwift.SignerProtocol {
+    var algorithm: JOSESwift.SignatureAlgorithm
+    let signature: Data
+    
+    init(signature: Data, algorithm: JOSESwift.SignatureAlgorithm) {
+        self.algorithm = algorithm
+        self.signature = signature
+    }
+    
+    func sign(_ signingInput: Data) throws -> Data {
+        return signature
     }
 }
