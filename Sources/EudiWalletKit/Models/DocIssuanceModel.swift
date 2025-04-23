18312d2a0c95c315ed08ac78dc0d4bff2cf9adad
Nikita Koruts <nikitakoruts@gmail.com>
2025-04-23 15:21:43 +0300

    Add DocIssuanceModel

---
+++ Sources/EudiWalletKit/Models/DocIssuanceModel.swift
@@ -0,0 +1,40 @@
+//
+//  DocIssuanceModel.swift
+//  EudiWalletKit
+//
+//  Created by Nikita Koruts on 26.03.2025.
+//
+
+import OpenID4VCI
+import MdocDataModel18013
+
+public struct DocIssuanceModel {
+	public let docType: String
+	public let dataFormats: [DocIssuanceDataFormat]
+	
+	public init(docType: String, dataFormats: [DocIssuanceDataFormat]) {
+		self.docType = docType
+		self.dataFormats = dataFormats
+	}
+}
+
+public struct DocIssuanceDataFormat {
+	public let format: DocDataFormat
+	public let identifier: String
+	
+	public init(format: DocDataFormat, identifier: String) {
+		self.format = format
+		self.identifier = identifier
+	}
+}
+
+public struct DocIssuanceRequest: Codable {
+	public let doctype: String
+	public let proofs: [DocIssuanceRequestProof]
+}
+
+public struct DocIssuanceRequestProof: Codable {
+	public let jwt: String
+	public let proofType: String
+	public let format: String
+}
