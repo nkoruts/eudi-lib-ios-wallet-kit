2258cdd5f4cee2e76af0c7c290f6e43ed3c52279
Nikita Koruts <nikitakoruts@gmail.com>
2025-05-23 12:50:47 +0300

    Use docType instead inputDescrId

--- Sources/EudiWalletKit/Services/OpenId4VpService.swift
+++ Sources/EudiWalletKit/Services/OpenId4VpService.swift
@@ -181,7 +181,8 @@ public final class OpenId4VpService: @unchecked Sendable, PresentationService {
 			var inputToPresentations = [(String, VpToken.VerifiablePresentation)]()
 			// support sd-jwt documents
 			for (docId, nsItems) in itemsToSend {
-				guard let docType = idsToDocTypes[docId], let inputDescrId = inputDescriptorMap[docType] else { continue }
+				guard let docType = idsToDocTypes[docId] else { continue }
+                let inputDescrId = docType
 				if dataFormats[docId] == .cbor {
 					if docsCbor == nil { makeCborDocs() }
 					let itemsToSend1 = Dictionary(uniqueKeysWithValues: [(docId, nsItems)])
