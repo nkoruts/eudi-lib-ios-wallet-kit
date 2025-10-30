14bfd57088b844ce6be2d287c2e07a4bfd520501
Nikita Koruts <nikitakoruts@gmail.com>
2025-10-30 09:18:37 +0200

    Throw error from getCredentialIssuingConfiguration method

--- Sources/EudiWalletKit/Extensions/OpenId4VCIService+Ext.swift
+++ Sources/EudiWalletKit/Extensions/OpenId4VCIService+Ext.swift
@@ -10,6 +10,7 @@ import OpenID4VCI
 
 extension OpenId4VCIService {
 	func getCredentialIssuingConfiguration(docTypeIdentifier: DocTypeIdentifier, metadata: CredentialIssuerMetadata) async throws -> CredentialConfiguration {
+		guard let credentialIssuerURL = config.credentialIssuerURL else { throw WalletError(description: "credentialIssuerURL not found") }
 		let credentialIssuerIdentifier = try CredentialIssuerId(credentialIssuerURL)
 		let credentialConfiguration = try getCredentialConfiguration(credentialIssuerIdentifier: credentialIssuerIdentifier.url.absoluteString.replacingOccurrences(of: "https://", with: ""), issuerDisplay: metadata.display, credentialsSupported: metadata.credentialsSupported, identifier: docTypeIdentifier.configurationIdentifier, docType: docTypeIdentifier.docType, vct: docTypeIdentifier.vct, batchCredentialIssuance: metadata.batchCredentialIssuance)
 		return credentialConfiguration
