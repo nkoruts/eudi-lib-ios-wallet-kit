9bb30d45a5d5171f4a758a9f1545a1477a572d9a
Nikita Koruts <nikitakoruts@gmail.com>
2025-11-05 18:09:03 +0200

    Update custom issuing logic

--- Sources/EudiWalletKit/Extensions/EudiWallet+Ext.swift
+++ Sources/EudiWalletKit/Extensions/EudiWallet+Ext.swift
@@ -7,38 +7,44 @@
 
 import Foundation
 import OpenID4VCI
+import MdocDataModel18013
 
 extension EudiWallet {
-	public func issueDocument(parameters: DocIssuanceModel, metadata: CredentialIssuerMetadata, issueCredentials: ((DocIssuanceRequest) async throws -> [CredentialIssuanceResponse])) async throws {
+	public func issueDocument(parameters: DocIssuanceModel, metadata: CredentialIssuerMetadata, credentialOptions: CredentialOptions, issueCredentials: ((DocIssuanceRequest) async throws -> [CredentialIssuanceResponse])) async throws {
 		let docType = parameters.docType
-		var openId4VCIServices: [OpenId4VCIService] = []
+		var vciServices: [OpenId4VCIService] = []
 		var proofs: [DocIssuanceRequestProof] = []
+		var publicKeys: [Data] = []
 		var configurations: [CredentialConfiguration] = []
-//		for dataFormat in parameters.dataFormats {
-//			let docTypeIdentifier: DocTypeIdentifier = .identifier(dataFormat.identifier)
-//			let openId4VCIService = try await prepareIssuing(id: UUID().uuidString, docTypeIdentifier: docTypeIdentifier, displayName: nil, keyOptions: nil, disablePrompt: false, promptMessage: nil)
-//			openId4VCIServices.append(openId4VCIService)
-//			let credentialConfiguration = try await openId4VCIService.getCredentialIssuingConfiguration(docTypeIdentifier: docTypeIdentifier, metadata: metadata)
-//			let securityKeys = try await openId4VCIService.initSecurityKeys(algSupported: Set(credentialConfiguration.credentialSigningAlgValuesSupported))
-//			guard let proof = try await securityKeys.first?.getProof() else { continue }
-//			let requestProof = DocIssuanceRequestProof(jwt: proof, proofType: "jwk", format: dataFormat.format.description)
-//			proofs.append(requestProof)
-//			configurations.append(credentialConfiguration)
-//		}
-//		let issuanceRequest = DocIssuanceRequest(doctype: docType, proofs: proofs)
-//		let issuanceResponse = try await issueCredentials(issuanceRequest)
-//		for (index, response) in issuanceResponse.enumerated() {
-//			guard openId4VCIServices.count > index, configurations.count > index else { return }
-//			let openId4VCIService = openId4VCIServices[index]
-//			let issuanceOutcome = try await handleIssuanceResponse(response, configuration: configurations[index], openId4VCIService: openId4VCIService)
-//			let format = parameters.dataFormats[index].format
-//			_ = try await finalizeIssuing(issueOutcome: issuanceOutcome, docType: docType, format: format, issueReq: openId4VCIService.issueReq, openId4VCIService: openId4VCIService)
-//		}
+		for dataFormat in parameters.dataFormats {
+			let docTypeIdentifier: DocTypeIdentifier = .identifier(dataFormat.identifier)
+			guard let config = openID4VciConfigurations?.values.first else { throw WalletError(description: "No VCI configurations available") }
+			let vciService = try OpenId4VCIService(uiCulture: uiCulture, config: config, networking: networkingVci, storage: storage, storageService: storage.storageService)
+			vciServices.append(vciService)
+			try await vciService.prepareIssuing(id: UUID().uuidString, docTypeIdentifier: docTypeIdentifier, displayName: nil, credentialOptions: credentialOptions, keyOptions: nil, disablePrompt: false, promptMessage: nil)
+			let credentialConfiguration = try await vciService.getCredentialIssuingConfiguration(docTypeIdentifier: docTypeIdentifier, metadata: metadata)
+			let algSupported = Set(credentialConfiguration.credentialSigningAlgValuesSupported)
+			let (bindingKeys, pblcKeys) = try await vciService.initSecurityKeys(algSupported: algSupported)
+			guard let proof = try await bindingKeys.first?.getProof(), let publicKey = pblcKeys.first else { continue }
+			let requestProof = DocIssuanceRequestProof(jwt: proof, proofType: "jwk", format: dataFormat.format.description)
+			proofs.append(requestProof)
+			configurations.append(credentialConfiguration)
+			publicKeys.append(publicKey)
+		}
+		let issuanceRequest = DocIssuanceRequest(doctype: docType, proofs: proofs)
+		let issuanceResponse = try await issueCredentials(issuanceRequest)
+		for (index, response) in issuanceResponse.enumerated() {
+			guard vciServices.count > index, configurations.count > index, publicKeys.count > index else { return }
+			let vciService = vciServices[index]
+			let issuanceOutcome = try await handleIssuanceResponse(response, publicKey: publicKeys[index], configuration: configurations[index], openId4VCIService: vciService)
+			let format = parameters.dataFormats[index].format
+			_ = try await vciService.finalizeIssuing(issueOutcome: issuanceOutcome, docType: docType, format: format, issueReq: vciService.issueReq)
+		}
 	}
 	
-//	private func handleIssuanceResponse(_ issuanceResponse: CredentialIssuanceResponse, configuration: CredentialConfiguration, openId4VCIService: OpenId4VCIService) async throws -> IssuanceOutcome {
-//		guard let result = issuanceResponse.credentialResponses.first else { throw WalletError(description: "No credential response results available") }
-//		guard case .issued(_, let credential, _, _) = result else { throw WalletError(description: "Unsupported document status (deferred) ") }
-//		return try await openId4VCIService.handleCredentialResponse(credentials: [credential], format: nil, configuration: configuration)
-//	}
+	private func handleIssuanceResponse(_ issuanceResponse: CredentialIssuanceResponse, publicKey: Data, configuration: CredentialConfiguration, openId4VCIService: OpenId4VCIService) async throws -> IssuanceOutcome {
+		guard let result = issuanceResponse.credentialResponses.first else { throw WalletError(description: "No credential response results available") }
+		guard case .issued(_, let credential, _, _) = result else { throw WalletError(description: "Unsupported document status (deferred) ") }
+		return try await openId4VCIService.handleCredentialResponse(credentials: [credential], publicKeys: [publicKey], format: nil, configuration: configuration)
+	}
 }
