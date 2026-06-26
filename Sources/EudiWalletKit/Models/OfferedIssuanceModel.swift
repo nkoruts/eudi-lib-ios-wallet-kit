28881a5f4e25a192105e121144fb50f28640e38b
Nikita Koruts <76942708+nkoruts@users.noreply.github.com>
2026-06-26 12:23:29 +0300

    Set credentialOptions variable

--- Sources/EudiWalletKit/Models/OfferedIssuanceModel.swift
+++ Sources/EudiWalletKit/Models/OfferedIssuanceModel.swift
@@ -94,7 +94,7 @@ public struct OfferedDocModel: Sendable {
 	/// Credential metadata from issuer (display, claims, etc.)
 	public let credentialMetadata: ConfigurationCredentialMetadata?
 	// options for the credential
-	public let credentialOptions: CredentialOptions
+	public var credentialOptions: CredentialOptions
 	// key options
 	public let keyOptions: KeyOptions?
 
